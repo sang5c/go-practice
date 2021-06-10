d9f578196ea0861081f116656ef991ec85d18075
sang5c <skah321@gmail.com>
2021-06-10 22:13:20 +0900

    add "Return greetings for multiple people"

--- copy-document/hello/hello.go
+++ copy-document/hello/hello.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
-	// log.SetPrefix("greetings: ")
-	// log.SetFlags(0)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-	message, err := greetings.Hello("sang5c")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	message, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
