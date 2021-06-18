73b32aa2e647bba24a9324d3b7df71ff2ec7e2b0
sang5c <skah321@gmail.com>
2021-06-18 20:01:39 +0900

    add gin GET with parameter example

--- example/gin/gin_ex.go
+++ example/gin/gin_ex.go
@@ -7,20 +7,28 @@ import (
 
 // https://github.com/gin-gonic/gin
 func main() {
-	r := gin.Default() // 생성
-	r.GET("/get", get)
-	r.POST("/post", post)
-	r.Run() // 실행
-}
-
-func post(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"result": "good",
-	})
+	router := gin.Default() // 생성
+	router.GET("/get", get)
+	router.GET("/user/:name/*action", getWithParameter)
+	router.POST("/post", post)
+	router.Run() // 실행
 }
 
 func get(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{
 		"123": "test",
+	})
+}
+
+func getWithParameter(context *gin.Context) {
+	name := context.Param("name")
+	action := context.Param("action")
+	message := name + " is " + action
+	context.String(http.StatusOK, message)
+}
+
+func post(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"result": "good",
 	})
 }
