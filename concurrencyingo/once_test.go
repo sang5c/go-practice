f25003041010d9051aa0c90e7c2825f77e8e80e7
sang5c <skah321@gmail.com>
2021-07-07 01:46:02 +0900

    concurrency in go once, pool 예제 추가

---
+++ concurrencyingo/once_test.go
@@ -0,0 +1,53 @@
+package main_test
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+func TestOnce(t *testing.T) {
+	var count int
+	increment := func() {
+		count++
+	}
+
+	var once sync.Once
+
+	var increments sync.WaitGroup
+	increments.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer increments.Done()
+			once.Do(increment)
+		}()
+	}
+
+	increments.Wait()
+	fmt.Printf("Count is %d\n", count)
+
+	assert.Equal(t, 1, count)
+}
+
+func TestOnce2(t *testing.T) {
+	var count int
+	increment := func() { count++ }
+	decrement := func() { count-- }
+
+	var once sync.Once
+	once.Do(increment)
+	once.Do(decrement)
+
+	fmt.Printf("Count: %d\n", count)
+
+	assert.Equal(t, 1, count)
+}
+
+func TestOnceDeadlock(t *testing.T) {
+	var onceA, onceB sync.Once
+	var initB func()
+	initA := func() { onceB.Do(initB) }
+	initB = func() { onceA.Do(initA) }
+	onceA.Do(initA)
+}
---
+++ concurrencyingo/pool_test.go
@@ -0,0 +1,54 @@
+package main_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestPool(t *testing.T) {
+	myPool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Creating new instance.")
+			return struct{}{}
+		},
+	}
+
+	myPool.Get()
+	instance := myPool.Get()
+	myPool.Put(instance)
+	myPool.Get()
+}
+
+func TestPool2(t *testing.T) {
+	var numCalcsCreated int
+	calcPool := &sync.Pool{
+		New: func() interface{} {
+			numCalcsCreated++
+			mem := make([]byte, 1024)
+			return &mem
+		},
+	}
+
+	// 4KB로 풀 시작
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := numWorkers; i > 0; i-- {
+		go func() {
+			defer wg.Done()
+			mem := calcPool.Get().(*[]byte)
+			defer calcPool.Put(mem)
+			// 작업 진행
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("%d calculators were created.", numCalcsCreated)
+}
