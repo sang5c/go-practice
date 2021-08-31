9e0e71a07acaeea1a2b68029a59c83f49724e114
sang5c <skah321@gmail.com>
2021-08-31 14:45:47 +0900

    chapter04 prevent-leak

--- concurrencyingo/chapter04/prevent-leak/done-prevent-leak.go
+++ concurrencyingo/chapter04/prevent-leak/done-prevent-leak.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
+	donePreventLeak()
+	dontKnowStop()
+	stopUseDone()
+}
+
+func donePreventLeak() {
 	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
 		terminated := make(chan interface{})
 		go func() {
@@ -44,4 +51,52 @@ func main() {
 
 	<-terminated // join
 	fmt.Println("Done.")
+}
+
+func dontKnowStop() {
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited")
+			defer close(randStream)
+			for {
+				randStream <- rand.Int()
+			}
+		}()
+		return randStream
+	}
+
+	randStream := newRandStream()
+	fmt.Println("3 random ints: ")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+}
+
+func stopUseDone() {
+	newRandStream := func(done <-chan interface{}) <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited")
+			defer close(randStream)
+			for {
+				select {
+				case <-done:
+					return
+				case randStream <- rand.Int():
+				}
+			}
+		}()
+		return randStream
+	}
+
+	done := make(chan interface{})
+	randStream := newRandStream(done)
+	fmt.Println("3 random ints: ")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+	close(done)
+
+	time.Sleep(1 * time.Second)
 }
