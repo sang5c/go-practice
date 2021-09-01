84870c30a8d10ddfacecee0ae94f42f32b306bf1
sang5c <skah321@gmail.com>
2021-09-01 16:43:35 +0900

    chapter04 pipeline

---
+++ concurrencyingo/chapter04/pipeline/pipeline.go
@@ -0,0 +1,104 @@
+package main
+
+import "fmt"
+
+func main() {
+	all()    // 일괄처리
+	stream() // 스트림 지향
+	extend() // 좀더 모범 사례
+}
+
+func all() {
+	multiply := func(values []int, multiplier int) []int {
+		multipliedValues := make([]int, len(values))
+		for i, v := range values {
+			multipliedValues[i] = v * multiplier
+		}
+		return multipliedValues
+	}
+
+	add := func(values []int, additive int) []int {
+		addedValues := make([]int, len(values))
+		for i, v := range values {
+			addedValues[i] = v + additive
+		}
+		return addedValues
+	}
+
+	ints := []int{1, 2, 3, 4}
+	for _, v := range multiply(add(multiply(ints, 2), 1), 2) {
+		fmt.Println(v)
+	}
+}
+
+func stream() {
+	multiply := func(value, multiplier int) int {
+		return value * multiplier
+	}
+
+	add := func(value, additive int) int {
+		return value + additive
+	}
+
+	ints := []int{1, 2, 3, 4}
+	for _, v := range ints {
+		fmt.Println(multiply(add(multiply(v, 2), 1), 2))
+	}
+}
+
+func extend() {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int, len(integers))
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	multiply := func(done <-chan interface{}, intStream <-chan int, multiplier int) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	add := func(done <-chan interface{}, intStream <-chan int, additive int) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
+	}
+}
