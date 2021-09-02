0ba4e1b2266a5ff7c2bb31f9ccd55684aaa023b8
sang5c <skah321@gmail.com>
2021-09-02 13:33:41 +0900

    chapter04 fan-out fan-in

--- concurrencyingo/chapter04/fan-out-fan-in/fan-out-fan-in.go
+++ concurrencyingo/chapter04/fan-out-fan-in/fan-out-fan-in.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync"
 	"time"
 )
 
-// 참고: https://github.com/kat-co/concurrency-in-go-src/blob/master/concurrency-patterns-in-go/fan-out-fan-in/fig-fan-out-naive-prime-finder.go
-func before() {
-
-	take := func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+var (
+	take = func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
 		takeStream := make(chan interface{})
 		go func() {
 			defer close(takeStream)
@@ -24,7 +24,7 @@ func before() {
 		return takeStream
 	}
 
-	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+	repeatFn = func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
 		valueStream := make(chan interface{})
 		go func() {
 			defer close(valueStream)
@@ -39,8 +39,7 @@ func before() {
 		return valueStream
 	}
 
-	// 값을 int형으로 변환하여 스트림에 담는다.
-	toInt := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
+	toInt = func(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
 		intStream := make(chan int)
 		go func() {
 			defer close(intStream)
@@ -55,7 +54,7 @@ func before() {
 		return intStream
 	}
 
-	primeFinder := func(done <-chan interface{}, intStream <-chan int) <-chan interface{} {
+	primeFinder = func(done <-chan interface{}, intStream <-chan int) <-chan interface{} {
 		primeStream := make(chan interface{})
 		go func() {
 			defer close(primeStream)
@@ -81,19 +80,65 @@ func before() {
 		return primeStream
 	}
 
-	// s
-	rand := func() interface{} { return rand.Intn(50000000) }
+	fanIn = func(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+		var wg sync.WaitGroup
+		multiplexedStream := make(chan interface{})
+		multiplex := func(c <-chan interface{}) {
+			defer wg.Done()
+			for i := range c {
+				select {
+				case <-done:
+					return
+				case multiplexedStream <- i:
+				}
+			}
+		}
 
+		wg.Add(len(channels))
+		for _, c := range channels {
+			go multiplex(c)
+		}
+		go func() {
+			wg.Wait()
+			close(multiplexedStream)
+		}()
+
+		return multiplexedStream
+	}
+)
+
+// 참고: https://github.com/kat-co/concurrency-in-go-src/blob/master/concurrency-patterns-in-go/fan-out-fan-in/fig-fan-out-naive-prime-finder.go
+func main() {
 	done := make(chan interface{})
 	defer close(done)
-
+	makeRand := func() interface{} { return rand.Intn(50000000) }
 	start := time.Now()
-	randIntStream := toInt(done, repeatFn(done, rand))
-	fmt.Println("Primes:")
+	randIntStream := toInt(done, repeatFn(done, makeRand))
+
+	// fan-out fan-in 패턴 적용 전
+	fmt.Println("=== Before fan-out fan-in ===")
+	fmt.Println("Primes")
 	for prime := range take(done, primeFinder(done, randIntStream), 10) {
+		fmt.Printf("\t%d\n", prime)
+	}
+	fmt.Printf("Search took: %v\n", time.Since(start))
+
+	// 적용 후
+	fmt.Println("=== After fan-out fan-in ===")
+	start = time.Now()
+	numFinders := runtime.NumCPU()
+	fmt.Printf("Spinning up %d prime finders.\n", numFinders)
+	// fan-out
+	//var fanOut []chan interface{}
+	finders := make([]<-chan interface{}, numFinders)
+	fmt.Println("Primes: ")
+	for i := 0; i < numFinders; i++ {
+		finders[i] = primeFinder(done, randIntStream)
+	}
+
+	for prime := range take(done, fanIn(done, finders...), 10) {
 		fmt.Printf("\t%d\n", prime)
 	}
 
 	fmt.Printf("Search took: %v", time.Since(start))
-
 }
