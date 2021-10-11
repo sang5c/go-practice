63cb828af2ae5b3988b89da0eb5fcad9f14f464b
sang5c <skah321@gmail.com>
2021-10-11 17:00:05 +0900

    add ch05 speed limit example

---
+++ concurrencyingo/chapter05/copy/copied-request.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	doWork := func(done <-chan interface{}, id int, wg *sync.WaitGroup, result chan<- int) {
+		started := time.Now()
+		defer wg.Done()
+
+		// 랜덤한 작업 부하 시뮬레이션. 여러 상황에서 수행시간이 각각 다르다는 것을 표현하기위해 아래 랜덤 부하를 사용하였음.
+		simulatedLoadTime := time.Duration(1+rand.Intn(5)) * time.Second
+		select {
+		case <-done:
+		case <-time.After(simulatedLoadTime):
+		}
+
+		select {
+		case <-done:
+		case result <- id:
+		}
+
+		took := time.Since(started)
+		// 핸들러가 얼마나 오래걸리는지
+		if took < simulatedLoadTime {
+			took = simulatedLoadTime
+		}
+		fmt.Printf("%v took %v\n", id, took)
+	}
+
+	done := make(chan interface{})
+	result := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go doWork(done, i, &wg, result)
+	}
+
+	firstReturned := <-result
+	close(done)
+	wg.Wait()
+
+	fmt.Printf("Received an answer from #%v\n", firstReturned)
+}
---
+++ concurrencyingo/chapter05/limite-speed/multi-limiter.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"golang.org/x/time/rate"
+	"sort"
+	"time"
+)
+
+type RateLimiter interface {
+	Wait(ctx context.Context) error
+	Limit() rate.Limit
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
+func Open() *APIConnection {
+	secondLimit := rate.NewLimiter(Per(2, time.Second), 1)
+	minuteLimit := rate.NewLimiter(Per(10, time.Minute), 10)
+	return &APIConnection{
+		rateLimiter: MultiLimiter(secondLimit, minuteLimit),
+	}
+}
+
+type APIConnection struct {
+	rateLimiter RateLimiter
+}
+
+func (a *APIConnection) ReadFile(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
